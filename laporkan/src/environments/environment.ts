0ed7080d9e6a2f0c6c3b5c38d88ccf178e121f4e
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2024-01-08 21:19:04 +0700

    Add login and register components with templates and styles

---
+++ laporkan/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyC6o0vMzMvp0XH8xlRwPG1f0RYdWS5AR5Q",
+        authDomain: "sample-project-374ac.firebaseapp.com",
+        projectId: "sample-project-374ac",
+        storageBucket: "sample-project-374ac.appspot.com",
+        messagingSenderId: "996778188666",
+        appId: "1:996778188666:web:47b354885ecd5f1dfb40c3",
+        measurementId: "G-7XD94YR8XP"
+    }
+}
