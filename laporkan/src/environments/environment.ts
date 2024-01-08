0524498663e1a996a01eda76aa07d21df997e0dd
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2024-01-08 21:26:07 +0700

    Update Firebase configuration

--- laporkan/src/environments/environment.ts
+++ laporkan/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     firebaseConfig : {
-        apiKey: "AIzaSyC6o0vMzMvp0XH8xlRwPG1f0RYdWS5AR5Q",
-        authDomain: "sample-project-374ac.firebaseapp.com",
-        projectId: "sample-project-374ac",
-        storageBucket: "sample-project-374ac.appspot.com",
-        messagingSenderId: "996778188666",
-        appId: "1:996778188666:web:47b354885ecd5f1dfb40c3",
-        measurementId: "G-7XD94YR8XP"
+        apiKey: "AIzaSyCcPDPfL97MkHlMxgI_pt9C7wvNAlzYvNM",
+        authDomain: "laporkan-ed567.firebaseapp.com",
+        projectId: "laporkan-ed567",
+        storageBucket: "laporkan-ed567.appspot.com",
+        messagingSenderId: "777602158556",
+        appId: "1:777602158556:web:6b9a782ba2f2b71e354919",
+        measurementId: "G-J42J1HF8MV"
     }
 }
