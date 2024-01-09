01415639cc65d815c057d6d2ba18dccfc4879df3
IbraRizqy <rizqyibra@gmail.com>
2024-01-09 19:05:01 +0700

    Add register form

--- laporkan/src/app/pages/register/register.component.ts
+++ laporkan/src/app/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  form!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit() : void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
 }
