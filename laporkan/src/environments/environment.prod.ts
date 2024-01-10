a43dde3f8a1b9aaac39ba8a0f938cc0e5202a0c4
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2024-01-10 22:17:43 +0700

    Refactor firebaseConfig in environment.prod.ts

--- laporkan/src/environments/environment.prod.ts
+++ laporkan/src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
-//
+import { environment as baseEnvironment } from './environment';
+
 export const environment = {
-    firebaseConfig : {
-        apiKey: "AIzaSyCcPDPfL97MkHlMxgI_pt9C7wvNAlzYvNM",
-        authDomain: "laporkan-ed567.firebaseapp.com",
-        projectId: "laporkan-ed567",
-        storageBucket: "laporkan-ed567.appspot.com",
-        messagingSenderId: "777602158556",
-        appId: "1:777602158556:web:6b9a782ba2f2b71e354919",
-        measurementId: "G-J42J1HF8MV"
-    },
+    firebaseConfig: baseEnvironment.firebaseConfig,
     production: true
 }
