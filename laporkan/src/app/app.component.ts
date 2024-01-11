c679612fc0348af526bff8a31110950020a11059
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2024-01-11 20:53:29 +0700

    Add flowbite plugin and navbar component

--- laporkan/src/app/app.component.ts
+++ laporkan/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Laporkan';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
